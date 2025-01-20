fee77a47bf55f4c04c76ad3cda47b538cc9b97e4
darceymckelvey <darceymckelvey@gmail.com>
2025-01-20 14:06:48 -0400

    eslint and prettier setup and slowly working on finalizing navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { NavbarComponent } from "./shared/components/navbar/navbar.component";
-import { IconService } from "./icon.service";
-import { MatIconModule } from "@angular/material/icon";
-import { CommonModule } from "@angular/common";
+import { Component, OnInit } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from './shared/components/navbar/navbar.component'
+import { IconService } from './icon.service'
+import { MatIconModule } from '@angular/material/icon'
+import { CommonModule } from '@angular/common'
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, MatIconModule, CommonModule],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = "darceymckelvey";
+export class AppComponent implements OnInit {
+  title = 'darceymckelvey'
 
   constructor(private iconService: IconService) {}
 
   ngOnInit() {
-    this.iconService.registerIcons();
+    this.iconService.registerIcons()
   }
 }
--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,33 +1,30 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import {
-  ReactiveFormsModule,
-  FormsModule,
-  FormGroup,
-  FormBuilder,
-  Validators,
-} from "@angular/forms";
-import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { environment } from '../../../environments/environment';
 
 @Component({
-  selector: "app-contact",
+  selector: 'app-contact',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  templateUrl: "./contact.component.html",
-  styleUrl: "./contact.component.css",
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
   contactForm: FormGroup;
+
   isSubmitting = false;
-  successMessage = "";
-  errorMessage = "";
+
+  successMessage = '';
+
+  errorMessage = '';
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
-      name: ["", Validators.required],
-      email: ["", [Validators.required, Validators.email]],
-      message: ["", Validators.required],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
     });
   }
 
@@ -38,41 +35,43 @@ export class ContactComponent {
 
   hasError(fieldName: string, errorType: string): boolean {
     const control = this.contactForm.get(fieldName);
-    return control
-      ? control.hasError(errorType) && (control.dirty || control.touched)
-      : false;
+    return control ? control.hasError(errorType) && (control.dirty || control.touched) : false;
   }
 
   sendEmail() {
-      if (this.contactForm.valid) {
-          this.isSubmitting = true;
-          const formData = this.contactForm.value;
+    if (this.contactForm.valid) {
+      this.isSubmitting = true;
+      const formData = this.contactForm.value;
 
-          emailjs.send (
-              environment.emailJsServiceId,
-              environment.emailJsTemplateId,
-              {
-                  name: formData.name,
-                  email: formData.email,
-                  message: formData.message
-              },
-              environment.emailJsUserId
-          )
-              .then((response: EmailJSResponseStatus) => {
-                  this.successMessage = 'Your message has been sent!';
-                  this.errorMessage = '';
-                  this.contactForm.reset();
+      emailjs
+        .send(
+          environment.emailJsServiceId,
+          environment.emailJsTemplateId,
+          {
+            name: formData.name,
+            email: formData.email,
+            message: formData.message,
+          },
+          environment.emailJsUserId
+        )
+        .then(
+          (response: EmailJSResponseStatus) => {
+            this.successMessage = 'Your message has been sent!';
+            this.errorMessage = '';
+            this.contactForm.reset();
 
-                  setTimeout(() => {
-                        this.successMessage = '';
-                  }, 500);
-              }, (error) => {
-                  this.errorMessage = 'An error occurred, please try again later.';
-                  this.successMessage = '';
-              })
-              .finally(() => {
-                  this.isSubmitting = false;
-              });
-      }
+            setTimeout(() => {
+              this.successMessage = '';
+            }, 1000); // Clear success message after 1 second
+          },
+          (error) => {
+            this.errorMessage = 'An error occurred, please try again later.';
+            this.successMessage = '';
+          }
+        )
+        .finally(() => {
+          this.isSubmitting = false;
+        });
+    }
   }
 }
