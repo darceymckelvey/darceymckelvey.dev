ed717d82fc5d1b64bef80385676b230922bac424
darceymckelvey <darceymckelvey@gmail.com>
2025-01-20 14:36:13 -0400

    hire me link for button on home feature fixed

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: "app-home",
+  selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: "./home.component.html",
-  styleUrl: "./home.component.css",
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
